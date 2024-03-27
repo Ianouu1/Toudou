c8c44c3e723657ed8eb19b318063d9e1b674b58a
22206844 <courrierdsf@gmail.com>
2024-03-27 10:06:03 +0100

    Propreté de la fonction db, supression de contraintes inutiles..

--- src/dbInteraction.py
+++ src/dbInteraction.py
@@ -1,26 +1,18 @@
 import sqlite3
 import uuid
-from datetime import datetime
-
-
-def convert_string_to_date(date_str):
-    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
 
 def registerTask(id: str, task: str, description: str, date: str, status: bool):
     try:
-        con = sqlite3.connect(r"C:\Users\22206844\Desktop\toudou-0.1\data\mydata.db")
+        con = sqlite3.connect("data/mydata.db")
         cur = con.cursor()
-        print("co")
 
         cur.execute(
             "CREATE TABLE IF NOT EXISTS toudou(id TEXT PRIMARY KEY, task TEXT, description TEXT, enddate TEXT, status INTEGER)"
         )
-        # Pour le status c'est avec des 0 ou des 1 je crois
-        print("create table")
+        # Pour le status, un booléen est stocké avec 0 ou 1
 
         task_data = (str(id), task, description, date, status)
-        task_data2 = ("OUAIS", "task", "description", convert_string_to_date('2024-03-21 08:30:00'), int(0))
 
         cur.execute("INSERT INTO toudou (id, task, description, enddate, status) VALUES (?, ?, ?, ?, ?)", task_data)
         con.commit()
@@ -34,13 +26,13 @@ def registerTask(id: str, task: str, description: str, date: str, status: bool):
 
 def registerTaskTest():
     """
-    This function isn't useful.
-    It helped me to know how to insert data in the database.
+    Fonction inutile.
+    Elle m'a aidée a comprendre comment enregistrer des données dans la base de données.
     """
     test_id = str(uuid.uuid4())
     test_task = "Tache"
     test_desc = "Description"
-    test_date = convert_string_to_date('2024-03-21 08:30:00')
+    test_date = "2024-03-21 08:30:00"
 
     registerTask(test_id, test_task, test_desc, test_date, True)
     test_id = "ouais"
