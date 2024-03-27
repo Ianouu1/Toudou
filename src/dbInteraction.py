afb33573697ba2e35b3dad23c4572c947712bc91
22206844 <courrierdsf@gmail.com>
2024-03-27 09:13:51 +0100

    Avancement script enregistrer BD (WIP)

--- src/dbInteraction.py
+++ src/dbInteraction.py
@@ -1,15 +1,25 @@
 import sqlite3
+from datetime import datetime
+
+
+def convert_string_to_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
 def registerTask(id: str, task: str, description: str, date: str, status: bool):
     con = sqlite3.connect(r"C:\Users\22206844\Desktop\toudou-0.1\data\mydata.db")
     cur = con.cursor()
+    print("co")
 
-    cur.execute("CREATE TABLE IF NOT EXISTS toudou(id TEXT PRIMARY KEY, task TEXT, description TEXT, date TEXT, status TEXT)")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS toudou(id TEXT PRIMARY KEY, task TEXT, description TEXT, date TEXT, status INTEGER)"
+    )
     # Comment je dois enregistrer une date : YYYY-MM-DD HH:MM:SS.SSS
     # Pour le status c'est avec des 0 ou des 1 je crois
+    print("create table")
 
     task_data = (str(id), task, description, date, status)
-    task_data2 = ("OUAIS", "task", "description", '2024-03-21', "0")
+    task_data2 = ("OUAIS", "task", "description", convert_string_to_date('2024-03-21 08:30:00'), int(0))
     cur.execute("INSERT INTO toudou (id, task, description, date, status) VALUES (?, ?, ?, ?, ?)", task_data)
+    con.commit()
     print("Tâche enregistrée avec succès dans la base de données.")
     con.close()
