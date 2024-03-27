ee142d27ef7b559992483915257f92c59ec00435
22206844 <courrierdsf@gmail.com>
2024-03-27 10:29:10 +0100

    Ajout de la méthode pour modifier une tache

--- src/dbInteraction.py
+++ src/dbInteraction.py
@@ -4,6 +4,7 @@ import uuid
 
 def registerTask(id: str, task: str, description: str, date: str, status: bool):
     try:
+        # ----- DB Connexion ----- #
         con = sqlite3.connect("data/mydata.db")
         cur = con.cursor()
 
@@ -11,9 +12,10 @@ def registerTask(id: str, task: str, description: str, date: str, status: bool):
             "CREATE TABLE IF NOT EXISTS toudou(id TEXT PRIMARY KEY, task TEXT, description TEXT, enddate TEXT, status INTEGER)"
         )
         # Pour le status, un booléen est stocké avec 0 ou 1
+        # ----- ------------ ----- #
+
 
         task_data = (str(id), task, description, date, status)
-
         cur.execute("INSERT INTO toudou (id, task, description, enddate, status) VALUES (?, ?, ?, ?, ?)", task_data)
         con.commit()
         print("Tâche enregistrée avec succès dans la base de données.")
@@ -26,8 +28,8 @@ def registerTask(id: str, task: str, description: str, date: str, status: bool):
 
 def registerTaskTest():
     """
-    Fonction inutile.
-    Elle m'a aidée a comprendre comment enregistrer des données dans la base de données.
+    This function isn't useful.
+    It helped me to know how to insert data in the database.
     """
     test_id = str(uuid.uuid4())
     test_task = "Tache"
@@ -37,3 +39,31 @@ def registerTaskTest():
     registerTask(test_id, test_task, test_desc, test_date, True)
     test_id = "ouais"
     registerTask(test_id, test_task, test_desc, test_date, False)
+
+def updateTask(id: str, task: str, description: str, date: str, status: bool):
+    try:
+        # ----- DB Connexion ----- #
+        con = sqlite3.connect("data/mydata.db")
+        cur = con.cursor()
+
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS toudou(id TEXT PRIMARY KEY, task TEXT, description TEXT, enddate TEXT, status INTEGER)"
+        )
+        # Pour le status, un booléen est stocké avec 0 ou 1
+        # ----- ------------ ----- #
+        cur.execute("SELECT id FROM toudou WHERE id = ?", (str(id),))
+        existing_id = cur.fetchone()
+
+        if existing_id:
+            task_data = (task, description, date, status, str(id))
+            cur.execute("UPDATE toudou SET task = ?, description = ?, enddate = ?, status = ? WHERE id = ?", task_data)
+            con.commit()
+            print("Tâche modifiée avec succès dans la base de données.")
+        else:
+            print("L'ID est mauvais")
+
+    except sqlite3.Error as e:
+        print("Erreur lors de la modification de la tâche :", e)
+    finally:
+        if con:
+            con.close()
--- src/toudou.py
+++ src/toudou.py
@@ -27,11 +27,23 @@ def cli():
 @click.option("-d", "--description", prompt="Description de la tache", help="Add a description to a task")
 @click.option("-da", "--date", prompt="Date d'echeance de la tache (YYYY-MM-DD HH:MM:SS)", help="Add an end date to a task (YYYY-MM-DD HH:MM:SS)")
 @click.option("-s", "--status", prompt="Status de la tache (True, False)", help="Add the curent status of a task (True: if finished, False: if not finished)")
-def newTask(task: str, description: str, date: str, status: bool):
+def createTask(task: str, description: str, date: str, status: bool):
     todo = Todo(uuid.uuid4(), task, description, date, status)
     click.echo(todo)
     dbInteraction.registerTask(todo.id, todo.task, todo.description, todo.date, todo.status)
 
 @cli.command()
-def newTaskTest():
+def createTaskTest():
     dbInteraction.registerTaskTest()
+
+@cli.command()
+@click.option("-i", "--id", prompt="l'identifiant de votre tache", help="The id of a task")
+@click.option("-t", "--task", prompt="Votre tache", help="The task to remember.")
+@click.option("-d", "--description", prompt="Description de la tache", help="Add a description to a task")
+@click.option("-da", "--date", prompt="Date d'echeance de la tache (YYYY-MM-DD HH:MM:SS)", help="Add an end date to a task (YYYY-MM-DD HH:MM:SS)")
+@click.option("-s", "--status", prompt="Status de la tache (True, False)", help="Add the curent status of a task (True: if finished, False: if not finished)")
+def updateTask(id:str, task: str, description: str, date: str, status: bool):
+    todo = Todo(id, task, description, date, status)
+    click.echo(todo)
+    dbInteraction.updateTask(todo.id, todo.task, todo.description, todo.date, todo.status)
+
