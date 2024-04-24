f1e88af35206d778afed0ca5801115f0962d7160
22206844 <courrierdsf@gmail.com>
2024-04-24 11:12:10 +0200

    SQLAlchemy modifications méthode createTask

--- src/toudou/models.py
+++ src/toudou/models.py
@@ -57,13 +57,10 @@ class Todo:
             status
         )
 
-def createTask(id: str, task: str, description: str, date: str, status: bool) -> object:
-    task_data = (str(id), task, description, date, status)
-    with get_db() as db:
-        db.execute("INSERT INTO toudou (id, task, description, enddate, status) VALUES (?, ?, ?, ?, ?)", task_data)
-
-
-
+def createTask(id: str, task: str, description: str, date: str, status: bool) -> None:
+    with engine.connect() as conn:
+        conn.execute(todos_table.insert().values(id=str(id), task=task, description=description, enddate=datetime.strptime(date, "%Y-%m-%d %H:%M:%S"), status=status))
+        conn.commit()
 def updateTask(id: str, task: str, description: str, date: str, status: bool):
     with get_db() as db:
         existing_id = db.execute("SELECT id FROM toudou WHERE id = ?", (str(id),)).fetchone()
@@ -108,16 +105,17 @@ def createTaskTest():
     Also add some know data to the database, it's better for testing other functions
     """
     test_id = str(uuid.uuid4())
-    test_task = "Tache"
+    test_task = "TETZ2TETAZEAZGEBAZ"
     test_desc = "Description"
     test_date = "2024-03-21 08:30:00"
 
     createTask(test_id, test_task, test_desc, test_date, True)
-    test_id = "ouais"
-    createTask(test_id, test_task, test_desc, test_date, False)
-    test_id = "coucou"
-    createTask(test_id, test_task, test_desc, test_date, False)
-    test_id = "salut"
-    createTask(test_id, test_task, test_desc, test_date, False)
-    test_id = "lala"
-    createTask(test_id, test_task, test_desc, test_date, False)
+    # createTask(test_id, test_task, test_desc, test_date, True)
+    # test_id = "ouais"
+    # createTask(test_id, test_task, test_desc, test_date, False)
+    # test_id = "coucou"
+    # createTask(test_id, test_task, test_desc, test_date, False)
+    # test_id = "salut"
+    # createTask(test_id, test_task, test_desc, test_date, False)
+    # test_id = "lala"
+    # createTask(test_id, test_task, test_desc, test_date, False)
