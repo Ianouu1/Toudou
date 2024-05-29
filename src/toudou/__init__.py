4660ea3a143c803a16a1098b0a7e2df4d74f567c
Ianouu1 <courrierdsf@gmail.com>
2024-05-29 11:00:20 +0200

    Working env

--- src/toudou/__init__.py
+++ src/toudou/__init__.py
@@ -1,16 +1,18 @@
+import os
+
 from flask import Flask
+
 
 def create_app():
     app = Flask(__name__)
-    from toudou.views import web_ui #add api later
+    from toudou.views import web_ui  #add api later
     #app.register_blueprint(api)
     app.register_blueprint(web_ui)
-
+    app.config.from_prefixed_env(prefix="TOUDOU_FLASK")
     return app
-    
+
+
 config = dict(
-    DATABASE_URL="sqlite:///mydata.db",
-    DEBUG=True
-    # DATABASE_URL=os.getenv("TOUDOU_DATABASE_URL", ""),
-    # DEBUG=os.getenv("TOUDOU_DEBUG", "False") == "True"
+    DATABASE_URL=os.getenv("TOUDOU_DATABASE_URL", ""),
+    DEBUG=os.getenv("TOUDOU_DEBUG", "False") == "True"
 )
